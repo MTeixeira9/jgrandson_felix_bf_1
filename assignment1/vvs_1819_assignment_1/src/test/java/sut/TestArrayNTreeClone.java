1f592fd9b5a07515670a90b4d6d79712437ba127
catGuerreiro <catarina.canelas.guerreiro@gmail.com>
2019-04-17 16:21:50 +0100

    Testes Clone e ToList

---
+++ assignment1/vvs_1819_assignment_1/src/test/java/sut/TestArrayNTreeClone.java
@@ -0,0 +1,20 @@
+package sut;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class TestArrayNTreeClone {
+
+	@Test
+	public void test_clone() {
+		List<Integer> list = Arrays.asList(1,2,3);
+		ArrayNTree<Integer> v = new ArrayNTree<>(list,2);
+		ArrayNTree<Integer> clone = v.clone();
+		assertEquals(v, clone);
+	}
+
+}
---
+++ assignment1/vvs_1819_assignment_1/src/test/java/sut/TestArrayNTreeToList.java
@@ -0,0 +1,28 @@
+package sut;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TestArrayNTreeToList {
+
+	
+	@Test
+	public void test_toList() {
+		List<Integer> list = Arrays.asList(1,2,3);
+		ArrayNTree<Integer> v = new ArrayNTree<>(list,2); 
+		List<Integer> test = v.toList();
+		List<Integer> expect = new LinkedList();
+		expect.add(1);
+		expect.add(2);
+		expect.add(3);
+		assertEquals(expect, test);
+	}
+	
+
+}
