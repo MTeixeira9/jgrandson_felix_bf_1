aa285c5edb1bb44b97467dd8845bd796c76146ea
Djimaraes <guiguimaraes@live.com.pt>
2019-04-19 16:20:14 +0100

    equals

--- assignment1/vvs_1819_assignment_1/src/test/java/sut/TestArrayNTreeEquals.java
+++ assignment1/vvs_1819_assignment_1/src/test/java/sut/TestArrayNTreeEquals.java
@@ -3,6 +3,7 @@ package sut;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -12,59 +13,59 @@ class TestArrayNTreeEquals {
 	@Test
 	public void test_Equals_this() {
 
-		Object other = this;
-		boolean res = other.equals(other);
-		assertEquals(true, res);
-	}
-	
-	@Test
-	public void test_Equals_other() {
-		
 		List<Integer> l1 = Arrays.asList(1,2,3);
-		
-		ArrayNTree<Integer> v1 = new ArrayNTree<>(l1,2); 
-		Object other = this;
-		
-		boolean res = v1.equals(other);
-		assertEquals(false,res);
 
-	}
-	
-
-	@Test
-	public void test_Equals_2() {
-		List<Integer> l1 = Arrays.asList(1,2,3);
-		List<Integer> l2 = Arrays.asList(1,2,3,4);
-		
 		ArrayNTree<Integer> v1 = new ArrayNTree<>(l1,2); 
-		ArrayNTree<Integer> v2 = new ArrayNTree<>(l2,2); 
 		
-		boolean res = v1.equals(v2);
-		assertEquals(false,res);
-	}
-	/*
-	@Test
-	public void test_Equals_3() {
-		//List<Integer> l1 = Arrays.asList(1,2,3);
-		
-		ArrayNTree<Integer> v1 = new ArrayNTree<>(l1,2); 
-		ArrayNTree<Integer> v2 = new ArrayNTree<>(l1,2); 
-		
-		boolean res = v1.equals(v2);
-		
+		boolean res = v1.equals(v1);
 		assertEquals(true,res);
 	}
-	*/
-	
+
+	@Test
+	public void test_Equals_other() {
+
+		List<Integer> l1 = Arrays.asList(1,2,3);
+
+		ArrayNTree<Integer> v1 = new ArrayNTree<>(l1,2); 
+		Object other = this;
+
+		boolean res = v1.equals(other);
+		assertEquals(false,res);
+	}
+
+	@Test
+	public void test_Equals_equalsTrees() {
+		List<Integer> l1 = Arrays.asList(1,2,3);
+		List<Integer> l2 = Arrays.asList(1,2,3,4);
+
+		ArrayNTree<Integer> v1 = new ArrayNTree<>(l1,2); 
+		ArrayNTree<Integer> v2 = new ArrayNTree<>(l2,2); 
+
+		boolean res = v1.equals(v2);
+		assertEquals(false,res);
+	}
 	
 	/*
 	@Test
-	public void test_Equals_Empty() {
-		ArrayNTree<Integer> v = new ArrayNTree<>(2);
+	public void test_Equals_dentro_equalsTrees() {
+		List<Integer> l1 = Arrays.asList(1,2,3);
+		List<Integer> l2 = Arrays.asList(1,2,3);
+
+		ArrayNTree<Integer> one = new ArrayNTree<>(l1,2); 
+		ArrayNTree<Integer> other = new ArrayNTree<>(l2,2); 
 		
+		Iterator it1 = one.iterator();
+		Iterator it2 = other.iterator();
+		boolean res = true;
 		
-		boolean res = v.equals(null);
-		assertEquals(false,res);
-	}
-	*/
+		while(it1.hasNext() && it2.hasNext()) {
+			if(it1.next() != it2.next()) {
+				res = false;
+			}
+			it1.next();
+			it2.next();
+		}
+			assertEquals(false, res);
+	}*/
+
 }
